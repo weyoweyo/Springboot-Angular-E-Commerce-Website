7fce45c447f58c676b08701839fda1af56978ffa
weyoweyo <65795212+weyoweyo@users.noreply.github.com>
2022-01-09 01:42:46 -0500

    Add files via upload

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3jf0rolzRXOmcS5d7',
+        issuer: 'https://dev-92856491.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
